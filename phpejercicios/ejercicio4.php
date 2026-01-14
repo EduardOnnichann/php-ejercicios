fbed0e98ff5ceb1b6596acddc3e1d340647a1568
EduardOnnichann <eduard.reineth@gmail.com>
2026-01-14 08:51:29 +0100

    Add files via upload

---
+++ phpejercicios/ejercicio4.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+echo "EJERCICIO 4" . "<br>" . "<br>";
+
+$numero = rand(1, 6);
+ 
+ 
+$raiz = sqrt($numero);
+ 
+ 
+echo "The square root of $numero is $raiz";
+
+?>
+
+</body>
+</html>
